7bd35e55ad4400d1e82e5d14f693c566280eb116
Eduardo Purificacao <eduardo.barbelino@gmail.com>
2022-05-10 22:24:14 -0300

    Adicionado .gitignore para database

--- app-backend/database/migrations/2022_05_10_004324_create_pco_persons.php
+++ app-backend/database/migrations/2022_05_10_004324_create_pco_persons.php
@@ -28,6 +28,6 @@ return new class extends Migration
      */
     public function down()
     {
-        Schema::dropIfExists('pco_people');
+        #Schema::dropIfExists('pco_persons');
     }
 };
