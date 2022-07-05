33dc0acf5bd1aae19a285050dae71da26b9e19a9
Eduardo Purificacao <eduardo.barbelino@gmail.com>
2022-07-05 06:37:07 -0300

    Finished User crud

--- app-backend/app/Repositories/UserRepository.php
+++ app-backend/app/Repositories/UserRepository.php
@@ -3,11 +3,32 @@
 namespace App\Repositories;
 
 use App\Models\User;
+use Illuminate\Http\Response;
+
 
 class UserRepository extends AbstractCRUDRepository
 {
     public function __construct()
     {
         $this->model = app(User::class);
+    }
+
+    public function create(array $data): Response
+    {
+        $data['password'] = bcrypt($data['password']);
+
+        $user = $this->model->create($data);
+        $accessToken = $user->createToken($user->email)->accessToken;
+
+        return response(
+            [
+                'data' => [
+                    'user' => $user,
+                    'access_token' => $accessToken
+                ],
+                'message' => 'Successfully created.'
+            ],
+            201
+        );
     }
 }
